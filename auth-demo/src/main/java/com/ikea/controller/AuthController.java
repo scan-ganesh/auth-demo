f617439994d57c939125472970df6b55b18b4b78
scan-ganesh <48750992+scan-ganesh@users.noreply.github.com>
2019-10-29 17:33:41 +0100

    Update AuthController.java

--- auth-demo/src/main/java/com/ikea/controller/AuthController.java
+++ auth-demo/src/main/java/com/ikea/controller/AuthController.java
@@ -2,10 +2,11 @@ package com.ikea.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @RestController
 public class AuthController {
-
+    @CrossOrigin (origins = "http://localhost:4200", maxAge=3600)
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
