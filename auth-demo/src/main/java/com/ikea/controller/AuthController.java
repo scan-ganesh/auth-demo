9565462be1ea825e4920a3f6636423095d9f13e9
scan-ganesh <48750992+scan-ganesh@users.noreply.github.com>
2019-10-29 17:36:33 +0100

    Update AuthController.java

--- auth-demo/src/main/java/com/ikea/controller/AuthController.java
+++ auth-demo/src/main/java/com/ikea/controller/AuthController.java
@@ -2,7 +2,7 @@ package com.ikea.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.CrossOrigin ;
 
 @RestController
 public class AuthController {
